f5db4de0a0e36760975ff1dd1439be441287a1c0
alphabeach <juryandrewnathaniel.lebosada@tup.edu.ph>
2024-11-16 11:48:03 +0800

    pwned api

--- backend/api/functions/analysis.py
+++ backend/api/functions/analysis.py
@@ -1,5 +1,7 @@
 # This will be used for the Entropy calculation
 import math
+import hashlib
+import requests
 
 def calculate_entropy(password):
     """Calculate password entropy in bits."""
@@ -15,3 +17,18 @@ def calculate_entropy(password):
     # Entropy formula: length * log2(pool_size)
     entropy = length * math.log2(pool_size) if pool_size > 0 else 0
     return entropy
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    
+    url = 'https://api.pwnedpasswords.com/range/' + first5_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
+    
+    hashes = (line.split(':') for line in res.text.splitlines())
+    for h, count in hashes:
+        if h == tail:
+            return int(count)
+    return 0
