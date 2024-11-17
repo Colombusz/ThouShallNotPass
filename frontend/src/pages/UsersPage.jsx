de1c61eee608591f9231f9ee947b94f8fa122072
Micooo24 <mico.rabino24@gmail.com>
2024-11-18 03:41:07 +0800

    admin adjustment

--- frontend/src/pages/UsersPage.jsx
+++ frontend/src/pages/UsersPage.jsx
@@ -8,7 +8,7 @@ import StatCard from "../components/common/StatCard";
 import UsersTable from "../components/users/UsersTable";
 import UserGrowthChart from "../components/users/UserGrowthChart";
 import UserActivityHeatmap from "../components/users/UserActivityHeatmap";
-// import UserDemographicsChart from "../components/users/UserDemographicsChart";
+
 
 const UsersPage = () => {
     const [userStats, setUserStats] = useState({
@@ -71,7 +71,7 @@ const UsersPage = () => {
                 <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8'>
                     <UserGrowthChart />
                     <UserActivityHeatmap />
-                    {/* <UserDemographicsChart /> */}
+              
                 </div>
             </main>
         </div>
